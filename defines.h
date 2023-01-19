2e8086d41fc49f47a247c057bcdffca7e5c9a55c
Michael Isabella <michaelisabella1@gmail.com>
2023-01-19 00:36:05 -0500

    Further organized the state machines and function declarations.
    Going to start splitting the function prototypes and declarations
    into various header / c files. Will also figure out what each
    state machines have access to.

--- defines.h
+++ defines.h
@@ -1,33 +1,36 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+// #include <stdbool.h>
 #include <time.h>
 #include <ncurses.h>
 
-/*==============================================*
+/*==================================================*
 
-    Here lies the State Machine declarations. 
-        These should go in the ".h" files. 
+    Here lies all of  State Machine declarations
 
-*==============================================*/
+*==================================================*/
 
-typedef enum { Exploration_Move_Player, Exploration_EnemyPatrol } StateMachine_Exploration;
-typedef enum { Move_PlayerForward, Move_PlayerLeft, Move_PlayerRight, Move_PlayerDown } StateMachine_Exploration_Move;
-typedef enum { EnemyPatrol_Scan, EnemyPatrol_Fight } StateMachine_Exploration_EnemyPatrol;
+// Papa state machine: where all the big state machine containers live!
+typedef enum { Exploration, Battle, Attack, GameOver } stateMachine;
+
+typedef enum { Exploration_MovePlayer, Exploration_EnemyPatrol } stateMachine_Exploration;
+typedef enum { MovePlayer_Forward, MovePlayer_Left, MovePlayer_Right, MovePlayer_Down } stateMachine_Exploration_MovePlayer;
+typedef enum { EnemyPatrol_Scan, EnemyPatrol_Fight } stateMachine_Exploration_EnemyPatrol;
 // These are just potential options for the game. We don't necessarily need to program all of these right now.
-typedef enum { BattleMenu_Attack, BattleMenu_Magic, BattleMenu_Defend, BattleMenu_Items, BattleMenu_Escape } StateMachine_BattleMenu;
-typedef enum { Attack_HitCheck, Attack_DamageCalc, Attack_KillCheck } StateMachine_Attack;
-typedef enum { Attack_KillCheck_Loot} StateMachine_Attack_KillCheck;
-typedef enum { GameOver_Win, GameOver_Lose } StateMachine_GameOver;
-typedef enum { GameOver_WinCredits } StateMachine_GameOver_Win;
-typedef enum { GameOver_LoseCredits } StateMachine_GameOver_Lose;
+typedef enum { Menu_Attack, Menu_Magic, Menu_Defend, Menu_Items, Menu_Escape } stateMachine_Battle;
+typedef enum { Attack_HitCheck, Attack_DamageCalc, Attack_KillCheck } stateMachine_Attack;
+typedef enum { Attack_KillCheck_Loot} stateMachine_Attack_KillCheck;
+typedef enum { GameOver_Win, GameOver_Lose } stateMachine_GameOver;
+typedef enum { GameOver_WinCredits } stateMachine_GameOver_Win;
+typedef enum { GameOver_LoseCredits } stateMachine_GameOver_Lose;
 
-/*==============================================*
+/*==================================================*
 
-        Here lies the function prototypes. 
-        These should go in the ".h" files. 
+        The below is for the event call stack! 
+        It's a linked list used to cycle out 
+        messages and possibly actions too.
 
-*==============================================*/
+*==================================================*/
 
 // A linked list (LL) node to store a queue entry - for message counter
 struct QNode {
@@ -40,21 +43,114 @@ struct QNode {
 struct Queue {
     struct QNode *front, *rear; 
 };
- 
-// A utility function to create a new linked list node - specifically for message counter
-struct QNode* newNode(char *k) {
+
+/*==================================================*
+
+    Function prototypes and declarations required 
+    for event call stack and UI.
+
+*==================================================*/
+
+void displayMenu();
+void beginNCurses();
+void endNCurses();
+void clearAndMove(int row, int column);
+void printOut (char *queuedMessages);
+int enQueue(struct Queue* q, char *k);
+void deQueue(struct Queue* q);
+
+// Displays what available actions the player has to take.
+void displayMenu() {
+    mvprintw(0,0,"\'e\' - to attack\n");
+    mvprintw(1,0,"\'q\' - to quit\n");
+    clearAndMove(3, 0);
+    return; 
+}
+
+// Begins nCurses mode as well as enables noecho mode and sets seed to 0 for random
+void beginNCurses() {
+    initscr();                   // Start curses mode
+    noecho();
+    srand(time(0));              // Set the seed for rand to 0
+}
+
+// Ends nCurses mode
+void endNCurses() {
+    endwin();            // End curses mode 
+}
+
+// Clears the window and moves the cursor
+void clearAndMove(int row, int column) {
+    clrtoeol();
+    move(row, column);
+}
+
+// Allocates memory for a new node that temporary holds a node being passed through the call stack.
+struct QNode* newNode(char *key) {
     struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
-    temp->key = k; 
+    temp->key = key; 
     temp->next = NULL;
     return temp; 
 }
  
-// A utility function to create an empty queue stack
+// A utility function to create an empty call stack
 struct Queue* createQueue() {
     struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
     q->front = q->rear = NULL;
     return q; 
 }
+ 
+// Allocates memory for a new node and inserts it to the front of the call stack.
+int enQueue(struct Queue* q, char *key) {
+    // Create a new LL node
+    struct QNode* temp = newNode(key);
+ 
+    // If queue is empty, then new node is moved to the head,
+    // because it's empty the tail and head are the same.
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return 1;
+    }
+ 
+    // otherwise add a new node to the back and point the tail to null.
+    q->rear->next = temp;
+    q->rear = temp;
+    return 1;
+}
+
+// Deallocates memory required for the current node in the call stack.
+void deQueue(struct Queue* q) {
+    // If queue is empty, return NULL.
+    if (q->front == NULL){
+        return; }
+ 
+    // Store previous front and move front one node ahead
+    struct QNode* temp = q->front;
+ 
+    q->front = q->front->next;
+ 
+    // If front becomes NULL, then change rear also as NULL
+    if (q->front == NULL){
+        q->rear = NULL;
+    free(temp); } 
+}
+
+// Function to print strings passed in to it. Potentially need to  
+// integrate Entity* Target arg. so we can display characters names in strings.
+void printOut (char *queuedMessages){
+    char *messageToPrint = queuedMessages;
+        clearAndMove(0, 10);
+        mvprintw(4, 4, "%s\n", messageToPrint);
+}
+
+/*==================================================*
+
+        Below is everything needed for the 
+        character creation and stat management.
+        I might use a different way of organizing 
+        compartmentalizing everything below later.
+
+*==================================================*/
 
 // Class for stats
 typedef struct {
@@ -73,21 +169,103 @@ typedef struct {
 } Entity_Stats;
 
 // Classes are enumerated. Warrior = 0; Ranger = 1; etc. 
-typedef enum ClassEnum {
+typedef enum {
     WARRIOR,
     RANGER,
     MAGE,
     GOD_NERD,
-} class;
+} Entity_Class;
 
 // struct for creating new character stats
 typedef struct {
-    Entity_Stats characterStats;
-    class class;
+    Entity_Stats stats;
+    Entity_Class class;
     char name[20];
     char characterBio[50];
     char combat_menu_options;
 } Entity;
+
+Entity* newEntity(Entity_Class class, char name[50]);
+void setName(Entity *Target, char name[50]);
+
+// Creates player and sets class to establish what stats they have.
+Entity* newEntity(Entity_Class class, char name[50]) {
+// allocate memory to player pointers.
+Entity *tempEntity = malloc(sizeof(Entity));
+setName(tempEntity, name);
+
+// Assign stats based on the given class. 
+switch(class){
+    case WARRIOR:
+        tempEntity->stats.currenthp = 60;
+        tempEntity->stats.currentmp = 0;
+        tempEntity->stats.max_hp = 100;
+        tempEntity->stats.max_mp = 100;
+        tempEntity->stats.physical_armor = 10;
+        tempEntity->stats.magic_armor = 7;
+        tempEntity->stats.strength = 15;
+        tempEntity->stats.dexterity = 10;
+        tempEntity->stats.wisdom = 3;
+        tempEntity->stats.intelligence = 3;
+        tempEntity->stats.luck = 5;
+        tempEntity->class = WARRIOR;
+        break;
+    case RANGER:
+        tempEntity->stats.currenthp = 35;
+        tempEntity->stats.currentmp = 0;
+        tempEntity->stats.max_hp = 100;
+        tempEntity->stats.max_mp = 100;
+        tempEntity->stats.physical_armor = 10;
+        tempEntity->stats.magic_armor = 8;
+        tempEntity->stats.strength = 7;
+        tempEntity->stats.dexterity = 5;
+        tempEntity->stats.wisdom = 5;
+        tempEntity->stats.intelligence = 3;
+        tempEntity->stats.luck = 5;
+        tempEntity->class = RANGER;
+        break;
+    case MAGE:
+        tempEntity->stats.currenthp = 20;
+        tempEntity->stats.currentmp = 60;
+        tempEntity->stats.max_hp = 100;
+        tempEntity->stats.max_mp = 100;
+        tempEntity->stats.physical_armor = 4;
+        tempEntity->stats.magic_armor = 11;
+        tempEntity->stats.strength = 3;
+        tempEntity->stats.dexterity = 7;
+        tempEntity->stats.wisdom = 10;
+        tempEntity->stats.intelligence = 15;
+        tempEntity->stats.luck = 7;
+        tempEntity->class = MAGE;
+        break;
+    case GOD_NERD:
+        tempEntity->stats.currenthp = 100;
+        tempEntity->stats.currentmp = 100;
+        tempEntity->stats.max_hp = 100;
+        tempEntity->stats.max_mp = 100;
+        tempEntity->stats.physical_armor = 100;
+        tempEntity->stats.magic_armor = 100;
+        tempEntity->stats.strength = 100;
+        tempEntity->stats.dexterity = 100;
+        tempEntity->stats.wisdom = 100;
+        tempEntity->stats.intelligence = 100;
+        tempEntity->stats.luck = 100;
+        tempEntity->class = GOD_NERD;
+   default:
+        tempEntity->stats.max_hp = 100;
+        tempEntity->stats.max_mp = 100;
+        break;
+    }
+    return(tempEntity);     // return memory address of player.
+}
+
+// setName creates the name for our entity. Currently this function leaks memory   
+// due to strcpy which is not stable, however when the game transitions over to  
+// SGDK a different built-in function will be used. 
+void setName(Entity *Target, char name[50]) {
+    strcpy(Target->name, name);
+    return; 
+}
 
 /*==============================================*
 
@@ -97,13 +275,7 @@ typedef struct {
 *==============================================*/
 
 void displayStats(Entity *Target, int trigger);
-void setName(Entity *Target, char name[50]);
-Entity* newEntity(class class, char name[50]);
-void displayMenu();
 int system_DiceRoll(int maximum, int minimum, int add_to_roll, int subtract_from_roll);
-void clearAndMove(int row, int column);
-void beginNcurses();
-void endNcurses();
 bool hitMiss(Entity *Attacker, Entity *Target, float hitPercentage);
 float hitCalc(Entity *Attacker, Entity *Target);
 int damageCalc(Entity *Attacker, Entity *Target); 
@@ -112,12 +284,10 @@ int classBonus(Entity *Attacker, Entity *Target, int class);
 int criticalHit(Entity *Attacker, Entity *Target);
 bool areTheyDead(Entity *Target);
 int gameController();
-void printOut (char *queuedMessages);
 char* hitOrMissMessages(int trigger);
 char* deadMessage(int trigger);
 char* deadAndQuitMessage(int trigger);
-int enQueue(struct Queue* q, char *k);
-void deQueue(struct Queue* q);
+
 
 /*===================================================*
 
@@ -126,76 +296,7 @@ void deQueue(struct Queue* q);
 
 *===================================================*/
 
-// Creates player and sets class to establish what stats they have access to
-Entity* newEntity(class class, char name[50]) {
-// allocate memory to player pointers.
-Entity *tempEntity = malloc(sizeof(Entity));
-setName(tempEntity, name);
 
-// Assign stats based on the given class. 
-switch(class){
-    case WARRIOR:
-        tempEntity->characterStats.currenthp = 60;
-        tempEntity->characterStats.currentmp = 0;
-        tempEntity->characterStats.max_hp = 100;
-        tempEntity->characterStats.max_mp = 100;
-        tempEntity->characterStats.physical_armor = 10;
-        tempEntity->characterStats.magic_armor = 7;
-        tempEntity->characterStats.strength = 15;
-        tempEntity->characterStats.dexterity = 10;
-        tempEntity->characterStats.wisdom = 3;
-        tempEntity->characterStats.intelligence = 3;
-        tempEntity->characterStats.luck = 5;
-        tempEntity->class = WARRIOR;
-        break;
-    case RANGER:
-        tempEntity->characterStats.currenthp = 35;
-        tempEntity->characterStats.currentmp = 0;
-        tempEntity->characterStats.max_hp = 100;
-        tempEntity->characterStats.max_mp = 100;
-        tempEntity->characterStats.physical_armor = 10;
-        tempEntity->characterStats.magic_armor = 8;
-        tempEntity->characterStats.strength = 7;
-        tempEntity->characterStats.dexterity = 5;
-        tempEntity->characterStats.wisdom = 5;
-        tempEntity->characterStats.intelligence = 3;
-        tempEntity->characterStats.luck = 5;
-        tempEntity->class = RANGER;
-        break;
-    case MAGE:
-        tempEntity->characterStats.currenthp = 20;
-        tempEntity->characterStats.currentmp = 60;
-        tempEntity->characterStats.max_hp = 100;
-        tempEntity->characterStats.max_mp = 100;
-        tempEntity->characterStats.physical_armor = 4;
-        tempEntity->characterStats.magic_armor = 11;
-        tempEntity->characterStats.strength = 3;
-        tempEntity->characterStats.dexterity = 7;
-        tempEntity->characterStats.wisdom = 10;
-        tempEntity->characterStats.intelligence = 15;
-        tempEntity->characterStats.luck = 7;
-        tempEntity->class = MAGE;
-        break;
-    case GOD_NERD:
-        tempEntity->characterStats.currenthp = 100;
-        tempEntity->characterStats.currentmp = 100;
-        tempEntity->characterStats.max_hp = 100;
-        tempEntity->characterStats.max_mp = 100;
-        tempEntity->characterStats.physical_armor = 100;
-        tempEntity->characterStats.magic_armor = 100;
-        tempEntity->characterStats.strength = 100;
-        tempEntity->characterStats.dexterity = 100;
-        tempEntity->characterStats.wisdom = 100;
-        tempEntity->characterStats.intelligence = 100;
-        tempEntity->characterStats.luck = 100;
-        tempEntity->class = GOD_NERD;
-   default:
-        tempEntity->characterStats.max_hp = 100;
-        tempEntity->characterStats.max_mp = 100;
-        break;
-    }
-    return(tempEntity);     // return memory address of player.
-}
 
 // when trigger is passed a 1, then show stats
     // else if trigger is passed a 0, don't show stats
@@ -204,25 +305,13 @@ void displayStats (Entity *Target, int trigger) {
         clearAndMove(10, 0);
         clearAndMove(11, 0);
         clearAndMove(12, 0);
-        mvprintw(10,0,"\nName: %s\nHealth: %d\nMP: %d\n", Target->name, Target->characterStats.currenthp, Target->characterStats.currentmp); 
+        mvprintw(10,0,"\nName: %s\nHealth: %d\nMP: %d\n", Target->name, Target->stats.currenthp, Target->stats.currentmp); 
     } else if (trigger == 0) {
         clearAndMove(10, 0);
         clearAndMove(11, 0);
         clearAndMove(12, 0);
         clearAndMove(13, 0);
     } 
-}
-
-void setName(Entity *Target, char name[50]) {
-    strcpy(Target->name, name);
-    return; 
-}
-
-void displayMenu() {
-    mvprintw(0,0,"\'e\' - to attack\n");
-    mvprintw(1,0,"\'q\' - to quit\n");
-    clearAndMove(3, 0);
-    return; 
 }
 
 // Dice roll function to determine probabilities. Helpful for hitPercentage, damageCalc, etc.
@@ -239,27 +328,9 @@ int system_DiceRoll(int maximum, int minimum, int add_to_roll, int subtract_from
     return adjusted_roll; 
 }
 
-// Function to begin nCurses, exit noEcho mode and initialize srand
-void beginNcurses() {
-    initscr();                   // Start curses mode
-    noecho();
-    srand(time(0));              // Set the seed for rand to 0
-}
-
-// Function to end nCurses
-void endNcurses() {
-    endwin();            // End curses mode 
-}
-
-// Function to clear and display messages using nCurses print function 
-void clearAndMove(int row, int column) {
-    clrtoeol();
-    move(row, column);
-}
-
 // function to calculate how likely it is for an Attacker to hit an enemy
 float hitCalc(Entity *Attacker, Entity *Target) {
-    float hitPercentage = (Attacker->characterStats.dexterity) * (system_DiceRoll(6, 1, Attacker->characterStats.luck, Target->characterStats.dexterity));
+    float hitPercentage = (Attacker->stats.dexterity) * (system_DiceRoll(6, 1, Attacker->stats.luck, Target->stats.dexterity));
     return hitPercentage;
 }
 
@@ -278,14 +349,14 @@ bool hitMiss(Entity *Attacker, Entity *Target, float hitPercentage) {
 
 // Function to calculate how much damage an attacker is doing
 int damageCalc(Entity *Attacker, Entity *Target) {
-    int damage = (Attacker->characterStats.strength) * system_DiceRoll(6, 1, Attacker->characterStats.luck, Target->characterStats.physical_armor);
+    int damage = (Attacker->stats.strength) * system_DiceRoll(6, 1, Attacker->stats.luck, Target->stats.physical_armor);
     return damage;
 }
 
 // Function to calculate how much damage is dealt
 int damageDealt(Entity *Target, int damage) {
-    Target->characterStats.currenthp = Target->characterStats.currenthp - damage;
-    return Target->characterStats.currenthp;
+    Target->stats.currenthp = Target->stats.currenthp - damage;
+    return Target->stats.currenthp;
 }
 
 // Function to declare the class bonus
@@ -304,7 +375,7 @@ int classBonus (Entity *Attacker, Entity *Target, int class ) {
 // returns a bool to determine death
 bool areTheyDead(Entity *Target) {
     bool deadStatus;
-    if (Target->characterStats.currenthp <= 0) {
+    if (Target->stats.currenthp <= 0) {
         deadStatus = true;
     } else {
         deadStatus = false;
@@ -336,49 +407,6 @@ char* hitOrMissMessages(int trigger) {
     if (trigger == 1){
         return hit; } 
     return miss;
-}
-
-// Function to print strings passed to it. Potentially need to incorporate a Entity* Target arg. so we can
-// display characters names in strings
-void printOut (char *queuedMessages){
-    char *messageToPrint = queuedMessages;
-        clearAndMove(0, 10);
-        mvprintw(4, 4, "%s\n", messageToPrint);
-}
- 
-// The function to add a key k to q
-int enQueue(struct Queue* q, char *k) {
-    // Create a new LL node
-    struct QNode* temp = newNode(k);
- 
-    // If queue is empty, then new node is front and rear
-    // both
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return 1;
-    }
- 
-    // Add the new node at the end of queue and change rear
-    q->rear->next = temp;
-    q->rear = temp;
-    return 1;
-}
-
-// Function to remove a key from given queue q
-void deQueue(struct Queue* q) {
-    // If queue is empty, return NULL.
-    if (q->front == NULL){
-        return; }
- 
-    // Store previous front and move front one node ahead
-    struct QNode* temp = q->front;
- 
-    q->front = q->front->next;
- 
-    // If front becomes NULL, then change rear also as NULL
-    if (q->front == NULL){
-        q->rear = NULL;
-    free(temp); } 
 }
 
 // gameController function, much of the code below is tentative.
--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include "defines.h"
 
+// stateMachine gameState = Exploration;
+// stateMachine_Title titleState = Title_TitleScreen;
+// stateMachine_Exploration explorationState = Exploration_LoadLevel;
+// stateMachine_Exploration_Menu explorationMenuState = Exploration_Menu_Main;
+// stateMachine_Exploration_Menu_Items explorationMenuItemsState = Exploration_Menu_Items_Main;
+// stateMachine_Battle battleState = Battle_LoadLevel;
+// stateMachine_Battle_Menu battleMenuState = Battle_Menu_Main;
+// stateMachine_Battle_Menu_Attack battleMenuAttackState = Battle_Menu_Attack_SelectTarget;
+// stateMachine_Attack attackState = Attack_LoadLevel;
+// stateMachine_Attack_KillCheck attackKillCheckState = Attack_KillCheck_Check;
+// stateMachine_GameOver gameOverState = Gameover_Lose;
+// stateMachine_GameOver_Win gameOverWinState = Gameover_Win_Animation;
+// stateMachine_GameOver_Lose gameOverLoseState = Gameover_Lose_Animation;
+// stateMachine_MainMenu mainMenuState = MainMenu_Display;
+
+bool stateInitialize = TRUE;
+
 int main () {
     beginNcurses();
     while (1){
