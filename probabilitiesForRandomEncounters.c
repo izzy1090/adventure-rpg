a6d0115e5c2eae88edc0a5624bf0e73ecd924a05
Michael Isabella <michaelisabella1@gmail.com>
2023-01-25 01:19:08 -0500

    Created a 3 x 2 matrix to play with.
    Need to figure out how to associate places on the matrix
    with entities on the battlefield.

--- probabilitiesForRandomEncounters.c
+++ probabilitiesForRandomEncounters.c
@@ -2,15 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include "defines.h"
 
 int system_DiceRoll(int maximum, int minimum, int add_to_roll, int subtract_from_roll);
 
-int main (void) {
-    srand(time(0));
-    int x = rand() % 6; // returns a number between 0 and 6
-    int y = rand() % 3; // returns a number between 0 and 3
-    int diceRoll = system_DiceRoll(6, 1, x, y);
-    printf("\nThe dice results are: %d\n", diceRoll);
+int main () {
+    Entity *Hero = newEntity(WARRIOR, "Michael");
+    int matrix[3][2] = { 
+        {"ten", 1}, {2, 3}, {4, 5}
+    };
+    
+    // for the indexes
+    int i, j;
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 2; j++){
+            printf("Element at x[%i][%i]: ", i, j);
+            printf("%d\n", matrix[i][j]);
+        }
+    }
+    // srand(time(0));
+    // int x = rand() % 6; // returns a number between 0 and 6
+    // int y = rand() % 3; // returns a number between 0 and 3
+    // int diceRoll = system_DiceRoll(6, 1, x, y);
+    // printf("\nThe dice results are: %d\n", diceRoll);
+
     return 0;
 }
 
